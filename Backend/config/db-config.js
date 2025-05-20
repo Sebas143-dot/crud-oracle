c7c5735da36e310b059f65ad947eabb7ee313d75
Carlos Puentestar <puentestarchamorrocarlosdaniel@gmail.com>
2025-05-20 09:45:25 -0500

    Refinación del Código de Backend

--- Backend/config/db-config.js
+++
@@ -1,8 +0,0 @@
-const oracledb = require('oracledb');
-require('dotenv').config();
-
-const dbConfig = {
-    connectString: process.env.ORACLE_CONNECT_STRING
-};
-
-module.exports = { oracledb, dbConfig };
