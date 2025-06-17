01fdffec06ac9a33370f2b8cf6a0d788f718e7e4
Sebastian-1232 <msrochap@utn.edu.ec>
2025-06-17 12:03:07 -0500

    Se quito la pagina para unicamente ejecutar codigo

--- Frontend/src/app/app.routes.ts
+++ Frontend/src/app/app.routes.ts
@@ -1,10 +1,11 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
-import { EjecutarCodigoComponent } from './ejecutar-codigo/ejecutar-codigo.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 import { authGuard } from './guards/auth.guard';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'ejecutar-codigo', component: EjecutarCodigoComponent, canActivate: [authGuard] },
-  { path: '', redirectTo: '/login', pathMatch: 'full' }
+  { path: 'dashboard', component: DashboardComponent, canActivate: [authGuard] },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },
+  { path: '**', redirectTo: 'login' }
 ];
