f227b6c8342813851cbbe22ca1ad9c4acb789598
Sebastian-1232 <msrochap@utn.edu.ec>
2025-06-25 07:29:01 -0500

    .

--- Backend/index.js
+++ Backend/index.js
@@ -24,4 +24,6 @@ app.get('/*', (req, res) => {
 });
 //#endregion
 
-app.listen(3000, () => console.log('Backend corriendo en http://localhost:3000'));
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Servidor corriendo en http://localhost:${PORT}`));
+
